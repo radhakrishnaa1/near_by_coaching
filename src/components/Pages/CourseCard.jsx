4e6692477751d91a923fe9dd384828cfcd30328d
radhakrishnaa1 <radhikasahu34@gmail.com>
2025-12-22 11:21:08 +0530

    course list data updates

--- src/components/Pages/CourseCard.jsx
+++ src/components/Pages/CourseCard.jsx
@@ -9,51 +9,64 @@ import { Avatar, Card, Typography } from "antd";
 const { Text } = Typography;
 const { Meta } = Card;
 const CourseCard = (props) => {
+  console.log(props);
 
- const courseData =[{
-    key:1,
-    label:"PCM 12th",
-    courseDiscription:"This is the Course description",
-    board:"CBSE",
-    medium:"English",
-    duration:"6 months",
-    fee:"4000",
-    timing:"6 AM",
-    mode:"online",
-    img:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-  }]
-
-console.log(props)
-
-return(  <Card
-    hoverable
-    style={{ width: 300 }}
-    cover={
-      <img
-        draggable={false}
-        alt="example"
-        src={props?.courseData?.img}
-      />
-    }
-    actions={[
-      <div>
-        <ClockCircleOutlined key="setting"> </ClockCircleOutlined> <Text>   {props?.courseData?.duration}</Text>
-      </div>,
-      <div>
-        <IconArrowJS size={20} fill="#3a3838ff" /> <Text key="setting"> </Text>
-       {props?.courseData?.fee}
-      </div>,
+  return (
+    <Card
+      hoverable
+      style={{ width: 300 }}
+      cover={
+        <img draggable={false} alt="example" src={props?.courseData?.img} />
+      }
+      actions={[
         <div>
-       {props?.courseData?.mode}
-         
+          <ClockCircleOutlined key="setting"> </ClockCircleOutlined>{" "}
+          <Text> {props?.courseData?.duration}</Text>
         </div>,
-    ]}
-  >
-    <Meta
-      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
-      title={props?.courseData?.label +" "+ props?.courseData?.board}
-      description={props?.courseData?.courseDescription}
-    />
-  </Card>)
-}
+        <div>
+          <IconArrowJS size={20} fill="#3a3838ff" />{" "}
+          <Text key="setting"> </Text>
+          {props?.courseData?.fee}
+        </div>,
+        <div>{props?.courseData?.mode}</div>,
+      ]}
+    >
+      <Meta
+        avatar={
+          <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+        }
+        title={props?.courseData?.label + " " + props?.courseData?.board}
+        description={props?.courseData?.courseDescription}
+      />
+    </Card>
+  );
+};
 export default CourseCard;
+
+// course_details;
+
+// course_duraton;
+
+// course_fee;
+
+// course_medium;
+
+// course_name;
+
+// courseid;
+
+// creation_date;
+
+// discount;
+
+// end_date;
+
+// max_student;
+
+// mode;
+
+// start_date;
+
+// status;
+
+// timing;
--- src/components/Pages/MyCourses.jsx
+++ src/components/Pages/MyCourses.jsx
@@ -1,73 +1,107 @@
 import React, { useState } from "react";
 import AuthLayout from "../Layouts/AuthLayout";
 import HomeCard from "../Pages/HomeCard";
-import { Divider,Avatar ,FloatButton,Card} from "antd";
-import CourseForm from "./Courseform"
+import { Divider, Avatar, FloatButton, Card } from "antd";
+import CourseForm from "./Courseform";
 import CourseCard from "./CourseCard";
-import CourseDetails from "./CourseDetails"
-import {PlusOutlined} from "@ant-design/icons";
+import CourseDetails from "./CourseDetails";
+import { PlusOutlined } from "@ant-design/icons";
+import axios from "axios";
+
 const MyCourses = () => {
-  const [size, setSize] = useState("large"); // default is 'middle'
+  const [size, setSize] = useState("large");
+  const [courseList, setCourseList] = useState("large"); // default is 'middle'
+  // default is 'middle'
+
+  React.useEffect(() => {
+    getCourseList();
+  }, []);
+  const getCourseList = () => {
+    axios({
+      method: "get",
+      url: `http://localhost:3004/getCourseDetails`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        setCourseList(response.data);
+        console.log("notes data", response.data);
+      })
+      .catch(() => {});
+  };
+
   
-  const courseData =[{
-    key:1,
-    label:"PCM 12th",
-    courseDescription:"This is the Course description",
-    board:"CBSE",
-    medium:"English",
-    duration:"6 months",
-    fee:"4000",
-    timing:"6 AM",
-    mode:"Online",
-    active:true,
-    status:"",
-    maxlimit:30,
-    img:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-  },
-{
-    key:2,
-    label:"10th Board",
-    courseDescription:"This is the Course description",
-    board:"CG Board",
-    medium:"English",
-    duration:"6 months",
-    fee:"4000",
-    timing:"6 AM",
-    mode:"Offline",
-    active:true,
-    maxlimit:45,
-    img:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-  },
-  {
-    key:3,
-    label:"PCM 12th",
-    courseDescription:"This is the Course description",
-    board:"CBSE",
-    medium:"English",
-    duration:"6 months",
-    fee:"4000",
-    timing:"6 AM",
-    mode:"Online",
-    active:true,
-    maxlimit:60,
-    img:"https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-  }
-]
+  const courseData = [
+    {
+      key: 1,
+      label: "PCM 12th",
+      courseDescription: "This is the Course description",
+      board: "CBSE",
+      medium: "English",
+      duration: "6 months",
+      fee: "4000",
+      timing: "6 AM",
+      mode: "Online",
+      active: true,
+      status: "",
+      maxlimit: 30,
+      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    },
+    {
+      key: 2,
+      label: "10th Board",
+      courseDescription: "This is the Course description",
+      board: "CG Board",
+      medium: "English",
+      duration: "6 months",
+      fee: "4000",
+      timing: "6 AM",
+      mode: "Offline",
+      active: true,
+      maxlimit: 45,
+      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    },
+    {
+      key: 3,
+      label: "PCM 12th",
+      courseDescription: "This is the Course description",
+      board: "CBSE",
+      medium: "English",
+      duration: "6 months",
+      fee: "4000",
+      timing: "6 AM",
+      mode: "Online",
+      active: true,
+      maxlimit: 60,
+      img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+    },
+  ];
 
   return (
     <AuthLayout>
-      <div style={{paddingTop:30,paddingBottom:30,display:"flex",justifyContent:"space-around"}}>
-     {courseData?.map((data,id)=>{
-            return <CourseCard courseData={data} />
-     }) 
-}
+      <div
+        style={{
+          paddingTop: 30,
+          paddingBottom: 30,
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        {courseData?.map((data, id) => {
+          return <CourseCard courseData={data} />;
+        })}
 
- <Card style={{ width: 300 ,textAlign:"center",paddingTop:100}}>
-      <Avatar style={{ backgroundColor: '#fde3cf', color: '#f56a00' }} size={64} icon={<PlusOutlined />} />
-    <p style={{paddingTop:20,fontFamily:"poppins"}}>Add Courses</p>
-  </Card>
-</div>
-      <CourseForm/>
+        <Card style={{ width: 300, textAlign: "center", paddingTop: 100 }}>
+          <Avatar
+            style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+            size={64}
+            icon={<PlusOutlined />}
+          />
+          <p style={{ paddingTop: 20, fontFamily: "poppins" }}>Add Courses</p>
+        </Card>
+      </div>
+      <CourseForm />
       <CourseDetails></CourseDetails>
     </AuthLayout>
   );
