0628aa60bde1aa8a306141d006b5994af9e47368
radhakrishnaa1 <radhikasahu34@gmail.com>
2025-12-29 17:06:32 +0530

    updates

---
+++ src/constants/data.jsx
@@ -0,0 +1,314 @@
+[
+  {
+    value: "35",
+    name: "ANDAMAN AND NICOBAR ISLANDS",
+  },
+  {
+    value: "28",
+    name: "ANDHRA PRADESH",
+  },
+  {
+    value: "12",
+    name: "ARUNACHAL PRADESH",
+  },
+  {
+    value: "18",
+    name: "ASSAM",
+  },
+  {
+    value: "10",
+    name: "BIHAR",
+  },
+  {
+    value: "4",
+    name: "CHANDIGARH",
+  },
+  {
+    value: "22",
+    name: "CHHATTISGARH",
+  },
+  {
+    value: "26",
+    name: "DADRA AND NAGAR HAVELI",
+  },
+  {
+    value: "25",
+    name: "DAMAN AND DIU",
+  },
+  {
+    value: "7",
+    name: "DELHI",
+  },
+  {
+    value: "30",
+    name: "GOA",
+  },
+  {
+    value: "24",
+    name: "GUJARAT",
+  },
+  {
+    value: "6",
+    name: "HARYANA",
+  },
+  {
+    value: "2",
+    name: "HIMACHAL PRADESH",
+  },
+  {
+    value: "1",
+    name: "JAMMU AND KASHMIR",
+  },
+  {
+    value: "20",
+    name: "JHARKHAND",
+  },
+  {
+    value: "29",
+    name: "KARNATAKA",
+  },
+  {
+    value: "32",
+    name: "KERALA",
+  },
+  {
+    value: "31",
+    name: "LAKSHADWEEP",
+  },
+  {
+    value: "23",
+    name: "MADHYA PRADESH",
+  },
+  {
+    value: "27",
+    name: "MAHARASHTRA",
+  },
+  {
+    value: "14",
+    name: "MANIPUR",
+  },
+  {
+    value: "17",
+    name: "MEGHALAYA",
+  },
+  {
+    value: "15",
+    name: "MIZORAM",
+  },
+  {
+    value: "13",
+    name: "NAGALAND",
+  },
+  {
+    value: "21",
+    name: "ODISHA",
+  },
+  {
+    value: "34",
+    name: "PUDUCHERRY",
+  },
+  {
+    value: "3",
+    name: "PUNJAB",
+  },
+  {
+    value: "8",
+    name: "RAJASTHAN",
+  },
+  {
+    value: "11",
+    name: "SIKKIM",
+  },
+  {
+    value: "33",
+    name: "TAMIL NADU",
+  },
+  {
+    value: "36",
+    name: "TELANGANA",
+  },
+  {
+    value: "16",
+    name: "TRIPURA",
+  },
+  {
+    value: "9",
+    name: "UTTAR PRADESH",
+  },
+  {
+    value: "5",
+    name: "UTTARAKHAND",
+  },
+  {
+    value: "19",
+    name: "WEST BENGAL",
+  },
+];
+
+[
+  {
+    value: "382",
+    name: "कबीरधाम",
+    type: "08",
+  },
+  {
+    value: "381",
+    name: "कांकेर",
+    type: "14",
+  },
+  {
+    value: "643",
+    name: "कोंडागांव",
+    type: "24",
+  },
+  {
+    value: "383",
+    name: "कोरबा",
+    type: "05",
+  },
+  {
+    value: "384",
+    name: "कोरिया",
+    type: "01",
+  },
+  {
+    value: "759",
+    name: "खैरागढ़ छुईखदान गंडई",
+    type: "30",
+  },
+  {
+    value: "645",
+    name: "गरियाबंद",
+    type: "20",
+  },
+  {
+    value: "734",
+    name: "गौरेला पेंड्रा मारवाही",
+    type: "28",
+  },
+  {
+    value: "380",
+    name: "जशपुर",
+    type: "03",
+  },
+  {
+    value: "379",
+    name: "जांजगीर-चाम्पा",
+    type: "06",
+  },
+  {
+    value: "376",
+    name: "दन्तेवाड़ा",
+    type: "16",
+  },
+  {
+    value: "378",
+    name: "दुर्ग",
+    type: "10",
+  },
+  {
+    value: "377",
+    name: "धमतरी",
+    type: "13",
+  },
+  {
+    value: "637",
+    name: "नारायणपुर",
+    type: "17",
+  },
+  {
+    value: "649",
+    name: "बलरामपुर",
+    type: "26",
+  },
+  {
+    value: "644",
+    name: "बलौदा बाजार",
+    type: "19",
+  },
+  {
+    value: "374",
+    name: "बस्तर",
+    type: "15",
+  },
+  {
+    value: "646",
+    name: "बालोद",
+    type: "22",
+  },
+  {
+    value: "375",
+    name: "बिलासपुर",
+    type: "07",
+  },
+  {
+    value: "636",
+    name: "बीजापुर",
+    type: "18",
+  },
+  {
+    value: "650",
+    name: "बेमेतरा ",
+    type: "23",
+  },
+  {
+    value: "760",
+    name: "मनेंद्रगढ़-चिरमिरी-भरतपुर",
+    type: "",
+  },
+  {
+    value: "385",
+    name: "महासमुंद",
+    type: "12",
+  },
+  {
+    value: "647",
+    name: "मुंगेली",
+    type: "21",
+  },
+  {
+    value: "761",
+    name: "मोहला-मानपुर-अंबागढ़ चौकी",
+    type: "29",
+  },
+  {
+    value: "388",
+    name: "राजनांदगांव",
+    type: "09",
+  },
+  {
+    value: "386",
+    name: "रायगढ़",
+    type: "04",
+  },
+  {
+    value: "387",
+    name: "रायपुर",
+    type: "11",
+  },
+  {
+    value: "762",
+    name: "सक्ती",
+    type: "",
+  },
+  {
+    value: "389",
+    name: "सरगुजा",
+    type: "02",
+  },
+  {
+    value: "763",
+    name: "सारंगढ़-बिलाईगढ़",
+    type: "31",
+  },
+  {
+    value: "642",
+    nzame: "सुकमा",
+    type: "25",
+  },
+  {
+    value: "648",
+    name: "सूरजपुर",
+    type: "27",
+  },
+];
